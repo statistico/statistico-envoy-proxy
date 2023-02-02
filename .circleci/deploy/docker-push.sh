b4e57445fb5769ca3d8b9bf4ecd17710f60985fa
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-02-02 17:04:56 +0000

    Release docker images

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -4,5 +4,5 @@ set -e
 
 aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION | bash
 
-docker tag "statisticoenvoyproxy_envoy" "$AWS_ECR_ACCOUNT_URL/statistico-envoy-proxy:$CIRCLE_SHA1"
+docker tag "statistico-envoy-proxy-envoy" "$AWS_ECR_ACCOUNT_URL/statistico-envoy-proxy:$CIRCLE_SHA1"
 docker push "$AWS_ECR_ACCOUNT_URL/statistico-envoy-proxy:$CIRCLE_SHA1"
--- .circleci/deploy/docker-save.sh
+++ .circleci/deploy/docker-save.sh
@@ -4,4 +4,4 @@ set -e
 
 mkdir -p /tmp/workspace/docker-cache
 
-docker save -o /tmp/workspace/docker-cache/statisticoenvoyproxy_envoy.tar statistico-envoy-proxy_envoy:latest
+docker save -o /tmp/workspace/docker-cache/statisticoenvoyproxy_envoy.tar statistico-envoy-proxy-envoy:latest
